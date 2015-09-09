7b03b8a55bd71744cbd567ff5609b3b2ecdf0204
Tim 'mithro' Ansell <mithro@mithis.com>
2015-09-09 12:20:41 +1000

    Adding gcc for lm32-elf.

---
+++ gcc-lm32-elf/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -x
+set -e
+
+lm32-elf-as --version
+
+rm -rf libstdc++-v3
+mkdir build
+cd build
+../configure \
+        --prefix=$PREFIX \
+        --with-gmp=$PREFIX \
+        --with-mpfr=$PREFIX \
+        --with-mpc=$PREFIX \
+        --with-isl=$PREFIX \
+        --with-cloog=$PREFIX \
+	--target=lm32-elf \
+	--enable-languages="c,c++" \
+	--disable-libgcc \
+	--disable-libssp
+
+make -j$CPU_COUNT
+make install
