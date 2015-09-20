a466a5b49e337f7715e171c440ea3578170d5b49
Tim 'mithro' Ansell <mithro@mithis.com>
2015-09-20 18:09:13 +1000

    Build openocd from git.
    Plus make sure the usb-blaster mode we need is enabled.

--- openocd/build.sh
+++ openocd/build.sh
@@ -3,10 +3,13 @@
 set -x
 set -e
 
+./bootstrap
 mkdir build
 cd build
 ../configure \
   --prefix=$PREFIX \
+  --enable-usb-blaster-2 \
+  --enable-usb_blaster_libftdi \
 
 make -j$CPU_COUNT
 make install
