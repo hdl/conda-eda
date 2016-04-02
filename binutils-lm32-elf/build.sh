01e3f179f8b57244d17553b9f0c403a98200a1de
Tim 'mithro' Ansell <mithro@mithis.com>
2016-04-02 18:50:29 +1100

    Adding buildstr.

--- binutils-lm32-elf/build.sh
+++ binutils-lm32-elf/build.sh
@@ -3,3 +3,4 @@ cd build
 ../configure --target=lm32-elf --prefix=$PREFIX --enable-deterministic-archives
 make -j$CPU_COUNT
 make install-strip
+TZ=UTC date +%Y%m%d_%H%M%S > ../__conda_buildstr__.txt
