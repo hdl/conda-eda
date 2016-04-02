b7dbed67e8f5e5e84d5b9dba0ae29af2c41ad622
Tim 'mithro' Ansell <mithro@mithis.com>
2016-04-02 18:52:56 +1100

    Small reformat.

--- binutils-lm32-elf/build.sh
+++ binutils-lm32-elf/build.sh
@@ -1,6 +1,10 @@
 mkdir build
 cd build
-../configure --target=lm32-elf --prefix=$PREFIX --enable-deterministic-archives
+../configure \
+  --target=lm32-elf \
+  --prefix=$PREFIX \
+  --enable-deterministic-archives \
+
 make -j$CPU_COUNT
 make install-strip
 TZ=UTC date +%Y%m%d_%H%M%S > ../__conda_buildstr__.txt
