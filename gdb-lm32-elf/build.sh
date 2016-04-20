7706480cdacbf019a62037dc3d31a62a0588bc08
Tim 'mithro' Ansell <mithro@mithis.com>
2016-04-20 15:46:52 +1000

    Adding gdb for lm32

---
+++ gdb-lm32-elf/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Instructions from http://openrisc.io/newlib/building.html
+
+set -x
+set -e
+
+(
+mkdir build
+cd build
+../configure \
+  --target=lm32-elf \
+  --prefix=$PREFIX \
+ \
+  --disable-itcl \
+  --disable-tk \
+  --disable-tcl \
+  --disable-winsup \
+  --disable-gdbtk \
+  --disable-libgui \
+  --disable-rda \
+  --disable-sid \
+  --disable-sim \
+  --disable-lm32sim \
+  --with-sysroot \
+  --disable-newlib \
+  --disable-libgloss \
+  --disable-gas \
+  --disable-ld \
+  --disable-binutils \
+  --disable-gprof \
+  --with-system-zlib \
+ \
+  --disable-shared \
+  --enable-static \
+
+make -j$CPU_COUNT
+make install
+)
+
+$PREFIX/bin/lm32-elf-gdb --version
+$PREFIX/bin/lm32-elf-gdb --version 2>&1 | head -1 | sed -e's/.* //' > ./__conda_version__.txt
+TZ=UTC date +%Y%m%d_%H%M%S > ./__conda_buildstr__.txt
+TZ=UTC date +%Y%m%d%H%M%S > ./__conda_buildnum__.txt
