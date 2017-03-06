b20f77d47f7afa8b357d62b50364901b652941f6
Tim 'mithro' Ansell <mithro@mithis.com>
2017-03-06 17:07:34 +1100

    Adding sh2 toolchain for j-core.

---
+++ binutils-sh2-elf/build.sh
@@ -0,0 +1,12 @@
+mkdir build
+cd build
+../configure \
+  --target=sh2-elf \
+  --prefix=$PREFIX \
+  --enable-deterministic-archives \
+
+make -j$CPU_COUNT
+make install-strip
+touch .buildstamp
+TZ=UTC date +%Y%m%d_%H%M%S -r .buildstamp > ../__conda_buildstr__.txt
+TZ=UTC date +%Y%m%d%H%M%S  -r .buildstamp > ../__conda_buildnum__.txt
