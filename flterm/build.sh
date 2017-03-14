db8651003ff5ca9579aba6c99647aab9e6c07993
Tim 'mithro' Ansell <mithro@mithis.com>
2017-03-14 20:05:20 +1100

    Using the timvideos github as source for flterm.

--- flterm/build.sh
+++ flterm/build.sh
@@ -6,7 +6,7 @@ set -e
 make CC="gcc -I."
 make PREFIX=$PREFIX install
 
-git describe | sed -e's/-/_/g' -e's,^debian/0,0,' > ./__conda_version__.txt
+git describe | sed -e's/-/_/g' -e's/^v//' > ./__conda_version__.txt
 touch .buildstamp
 TZ=UTC date +%Y%m%d_%H%M%S -r .buildstamp > ../__conda_buildstr__.txt
 TZ=UTC date +%Y%m%d%H%M%S  -r .buildstamp > ../__conda_buildnum__.txt
