4231f24def15b421555842eb7b6a11cdc298a98f
Tim 'mithro' Ansell <mithro@mithis.com>
2017-03-25 22:20:07 +1100

    Force getting conda on CI.

--- get-conda.sh
+++ get-conda.sh
@@ -7,8 +7,8 @@ CONDA_PATH=${1:-~/conda}
 
 wget -c https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod a+x Miniconda3-latest-Linux-x86_64.sh
-if [ ! -d $CONDA_PATH ]; then
-        ./Miniconda3-latest-Linux-x86_64.sh -p $CONDA_PATH -b
+if [ ! -d $CONDA_PATH -o ! -z "$CI"  ]; then
+        ./Miniconda3-latest-Linux-x86_64.sh -p $CONDA_PATH -b -f
 fi
 export PATH=$CONDA_PATH/bin:$PATH
 conda update -y conda
