6158f7fd3799e9a07e055263f6f2d6967eedd0c1
Tim 'mithro' Ansell <mithro@mithis.com>
2017-04-04 11:55:26 +1000

    openocd: Adding few small things.

--- openocd/build.sh
+++ openocd/build.sh
@@ -2,11 +2,11 @@
 
 set -x
 set -e
-export GIT_AUTHOR_NAME="Conda Build"
-export GIT_AUTHOR_EMAIL="robot@timvideos.us"
-export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
-export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
-git commit -a -m "Changes for conda."
+#export GIT_AUTHOR_NAME="Conda Build"
+#export GIT_AUTHOR_EMAIL="robot@timvideos.us"
+#export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+#export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+#git commit -a -m "Changes for conda."
 
 ./bootstrap
 mkdir build
@@ -22,6 +22,8 @@ cd build
 make -j$CPU_COUNT
 make install
 
-./src/openocd --version 2>&1 | head -1 | sed -e's/-/_/g' -e's/.* 0\./0./' -e's/ .*//' > ../__conda_version__.txt
-./src/openocd --version 2>&1 | head -1 | sed -e's/[^(]*(//' -e's/)//' -e's/://g' -e's/-/_/g' > ../__conda_buildstr__.txt
+./src/openocd --version
+
+./src/openocd --version 2>&1 | head -1 | sed -e's/+dev-[0]\+/_/' -e's/-/_/g' -e's/.* 0\./0./' -e's/ .*//' > ../__conda_version__.txt
+./src/openocd --version 2>&1 | head -1 | sed -e's/[^(]*(//' -e's/)//' -e's/://g' -e's/-//g' -es'/[0-9][0-9][0-9][0-9]$/_\0/' > ../__conda_buildstr__.txt
 TZ=UTC date +%Y%m%d%H%M%S > ../__conda_buildnum__.txt
