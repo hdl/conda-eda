957f2bbca0289c9c4efa9f2cb492076b4c4a3c90
Tim 'mithro' Ansell <mithro@mithis.com>
2017-04-04 12:00:28 +1000

    openocd: More working on version output.

--- openocd/build.sh
+++ openocd/build.sh
@@ -8,6 +8,8 @@ set -e
 #export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 #git commit -a -m "Changes for conda."
 
+git status
+
 ./bootstrap
 mkdir build
 cd build
@@ -24,6 +26,6 @@ make install
 
 ./src/openocd --version
 
-./src/openocd --version 2>&1 | head -1 | sed -e's/+dev-[0]\+/_/' -e's/-/_/g' -e's/.* 0\./0./' -e's/ .*//' > ../__conda_version__.txt
+./src/openocd --version 2>&1 | head -1 | sed -e's/+dev-[0]\+/_/' -e's/-/_/g' -e's/.* 0\./0./' -e's/ .*//' -e's/_dirty//' > ../__conda_version__.txt
 ./src/openocd --version 2>&1 | head -1 | sed -e's/[^(]*(//' -e's/)//' -e's/://g' -e's/-//g' -es'/[0-9][0-9][0-9][0-9]$/_\0/' > ../__conda_buildstr__.txt
 TZ=UTC date +%Y%m%d%H%M%S > ../__conda_buildnum__.txt
