3359c7c06b484dbdd6f28ea4fe42f7536456e07f
Tim 'mithro' Ansell <mithro@mithis.com>
2017-10-22 01:38:00 -0400

    Adding iverilog.

---
+++ iverilog/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+set -x
+
+sh ./autoconf.sh
+./configure --prefix=$PREFIX
+
+make -j$CPU_COUNT
+make install
+
+$PREFIX/bin/iverilog -V
+$PREFIX/bin/iverilog -h || true
+
+cat > hello_world.v <<'EOF'
+module hello_world;
+initial begin
+	$display("Hello World!");
+	#10 $finish;
+end
+endmodule
+EOF
+$PREFIX/bin/iverilog -v hello_world.v
