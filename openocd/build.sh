142e1596a1e0eebadbb62d2fc3109b89f722202c
Tim 'mithro' Ansell <mithro@mithis.com>
2017-10-26 00:51:06 -0700

    openocd: Actually to build statically.

--- openocd/build.sh
+++ openocd/build.sh
@@ -13,8 +13,10 @@ git status
 ./bootstrap
 mkdir build
 cd build
-LDFLAGS="-l:libftdi.a -l:libusb.a -l:libusb-1.0.a -lpthread -ludev" ../configure \
+../configure \
   --prefix=$PREFIX \
+  --enable-static \
+  --disable-shared \
   --enable-usb-blaster-2 \
   --enable-usb_blaster_libftdi \
   --enable-jtag_vpi \
@@ -22,11 +24,21 @@ LDFLAGS="-l:libftdi.a -l:libusb.a -l:libusb-1.0.a -lpthread -ludev" ../configure
 
 
 make -j$CPU_COUNT
+
+echo "---------------------------"
+ldd ./src/openocd
+ls -l ./src/openocd
+du -h ./src/openocd
+echo "---------------------------"
+gcc -Wall -Wstrict-prototypes -Wformat-security -Wshadow -Wextra -Wno-unused-parameter -Wbad-function-cast -Wcast-align -Wredundant-decls -Werror -g -O2 -o src/openocd src/main.o -Wl,-Bstatic src/.libs/libopenocd.a ./jimtcl/libjim.a -lusb-1.0 -lusb -lftdi1 -lftdi -Wl,-Bdynamic -Wl,--no-whole-archive -ludev -lpthread -ldl -lm
+ldd ./src/openocd
+ls -l ./src/openocd
+du -h ./src/openocd
+echo "---------------------------"
+
 make install
 
 ./src/openocd --version
-ldd ./src/openocd
-
 ./src/openocd --version 2>&1 | head -1 | sed -e's/+dev-[0]\+/_/' -e's/-/_/g' -e's/.* 0\./0./' -e's/ .*//' -e's/_dirty//' > ../__conda_version__.txt
 ./src/openocd --version 2>&1 | head -1 | sed -e's/[^(]*(//' -e's/)//' -e's/://g' -e's/-//g' -es'/[0-9][0-9][0-9][0-9]$/_\0/' > ../__conda_buildstr__.txt
 TZ=UTC date +%Y%m%d%H%M%S > ../__conda_buildnum__.txt
