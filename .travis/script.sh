f2cfdee7a095d3f963eb107f10cfbd0c0bbc7a51
Tim 'mithro' Ansell <me@mith.ro>
2018-03-04 15:19:45 -0800

    travis: Making the log output more pretty.
     * Add colors.
     * Add travis folding.

---
+++ .travis/script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source .travis/common.sh
+set -e
+
+$SPACER
+
+start_section "conda.check" "${GREEN}Checking...${NC}"
+conda build --check $PACKAGE
+end_section "conda.check"
+
+$SPACER
+
+start_section "conda.build" "${GREEN}Building..${NC}"
+$CONDA_PATH/bin/python ./.travis-output.py output.log conda build $PACKAGE
+end_section "conda.build"
+
+$SPACER
+
+start_section "conda.build" "${GREEN}Installing..${NC}"
+conda install $CONDA_OUT
+end_section "conda.build"
+
+$SPACER
+
+start_section "conda.du" "${GREEN}Disk usage..${NC}"
+du -h $CONDA_OUT
+end_section "conda.du"
+
+$SPACER
+
+start_section "conda.clean" "${GREEN}Cleaning up..${NC}"
+conda clean -s --dry-run
+end_section "conda.clean"
+
+$SPACER
