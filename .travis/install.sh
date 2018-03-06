d99e659705c3a9b65a49d76c0828d3b18e7bcb0c
Tim 'mithro' Ansell <me@mith.ro>
2018-03-05 23:09:18 -0800

    travis: Output more git info.

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,9 +6,18 @@ set -e
 # Git repo fixup
 start_section "environment.git" "Setting up ${YELLOW}git checkout${NC}"
 set -x
+git fetch --unshallow || true
 git fetch --tags
 git submodule update --recursive --init
 git submodule foreach git submodule update --recursive --init
+$SPACER
+git remote -v
+git branch -v
+git branch -D master
+CURRENT_GITREV="$(git rev-parse HEAD)"
+git checkout -b master $CURRENT_GITREV
+git tag -l
+git status -v
 git describe --long
 set +x
 end_section "environment.git"
