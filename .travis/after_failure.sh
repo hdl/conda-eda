1850585695e558b07cee3738d6b2a8ec77e13f65
Tim 'mithro' Ansell <me@mith.ro>
2018-05-22 21:07:09 -0700

    travis: Fixing after_failure output.

--- .travis/after_failure.sh
+++ .travis/after_failure.sh
@@ -5,6 +5,9 @@ source $TRAVIS_BUILD_DIR/.travis/common.sh
 $SPACER
 
 start_section "failure.tail" "${RED}Failure output...${NC}"
+echo "Log is $(wc -l /tmp/output.log) lines long."
+echo "Displaying last 1000 lines"
+echo
 tail -n 1000 /tmp/output.log
 end_section "failure.tail"
 
@@ -21,5 +24,5 @@ done
 $SPACER
 
 start_section "failure.log.full" "${RED}Failure output.log...${NC}"
-cat output.log
+cat /tmp/output.log
 end_section "failure.log.full"
