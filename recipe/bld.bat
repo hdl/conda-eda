4d76f9d0172fcae70fe332e794de240d91f3c9e2
Isuru Fernando <isuruf@gmail.com>
2018-08-24 00:23:56 -0500

    Remove unnecessary workaround

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,6 @@ set LD=lld-link
 set CXX=clang++
 set CC=clang
 set ac_cv_have_decl__BitScanForward=yes
-cp %LIBRARY_PREFIX%/mingw-w64/bin/m2w64-make.exe %LIBRARY_PREFIX%/mingw-w64/bin/make.exe
 
 FOR /F "delims=" %%i IN ('cygpath.exe -u "%PREFIX%"') DO set "PREFIX=%%i"
 bash -lc "./configure --with-int=imath CFLAGS='-O3 -Dstrdup=_strdup'"
