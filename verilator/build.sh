f3e2dce225640952d0a98f87e62a4a541274757d
Tim 'mithro' Ansell <me@mith.ro>
2018-09-18 16:36:19 -0700

    verilator: Move to new template method.

--- verilator/build.sh
+++ verilator/build.sh
@@ -15,8 +15,3 @@ autoconf
 
 make
 make install
-
-git describe | sed -e's/-/_/g' -e's/^verilator_3_/3./' > ./__conda_version__.txt
-touch .buildstamp
-TZ=UTC date +%Y%m%d_%H%M%S -r .buildstamp > ../__conda_buildstr__.txt
-TZ=UTC date +%Y%m%d%H%M%S  -r .buildstamp > ../__conda_buildnum__.txt
