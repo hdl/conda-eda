cf91cd167ffbdd215c6925b0be787126b7ee0982
Tim 'mithro' Ansell <me@mith.ro>
2018-09-23 12:16:43 +0200

    travis: Don't produce config.log output after failure.

--- .travis/after_failure.sh
+++ .travis/after_failure.sh
@@ -16,15 +16,15 @@ end_section "failure.tail"
 
 $SPACER
 
-COUNT=0
-for i in $(find $BASE_PATH -name config.log); do
-	start_section "failure.log.$COUNT" "${RED}Failure output $i...${NC}"
-	cat $i
-	end_section "failure.log.$COUNT"
-	COUNT=$((COUNT+1))
-done
-
-$SPACER
+#COUNT=0
+#for i in $(find $BASE_PATH -name config.log); do
+#	start_section "failure.log.$COUNT" "${RED}Failure output $i...${NC}"
+#	cat $i
+#	end_section "failure.log.$COUNT"
+#	COUNT=$((COUNT+1))
+#done
+#
+#$SPACER
 
 start_section "failure.log.full" "${RED}Failure output.log...${NC}"
 cat /tmp/output.log
