e6b8598173f82abc65738948f3b1320e67cc72b8
Tim 'mithro' Ansell <me@mith.ro>
2019-02-25 09:10:45 -0800

    Exclude SRC_DIR as well.

--- .travis-output.py
+++ .travis-output.py
@@ -38,7 +38,7 @@ def output_line(line_bits, last_skip):
     skip = False
     sline = sline.split('make')[-1]
 
-  if re.search("[0-9]+\.[0-9]+", line) and "PACKAGE_" not in line:
+  if re.search("[0-9]+\.[0-9]+", line) and "PACKAGE_" not in line and "SRC_DIR" not in line:
     skip = False
 
   if len(sline) > 1:
