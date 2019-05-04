02709489a526debe777579c7e594431307de9a74
Tim 'mithro' Ansell <me@mith.ro>
2019-05-04 10:05:20 -0700

    nextpnr: Only use 1 CPU on travis.
    Otherwise it runs out of memory.

--- nextpnr/build.sh
+++ nextpnr/build.sh
@@ -5,8 +5,10 @@ set -x
 
 if [ x"$TRAVIS" = xtrue ]; then
 	CPU_COUNT=1
+else
+	CPU_COUNT=$(nproc)
 fi
 
 cmake -DARCH=ice40 -DBUILD_GUI=OFF -DICEBOX_ROOT=${PREFIX}/share/icebox -DCMAKE_INSTALL_PREFIX=/ -DENABLE_READLINE=No .
-make -j$(nproc)
+make -j${CPU_COUNT}
 make DESTDIR=${PREFIX} install
