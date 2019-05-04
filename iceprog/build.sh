e28e875f0113bdf4097634ac77e8d6d719de0345
Tim 'mithro' Ansell <me@mith.ro>
2019-05-03 23:33:23 -0700

    Adding libftdi + libusb with new config.

--- iceprog/build.sh
+++ iceprog/build.sh
@@ -7,25 +7,25 @@ if [ x"$TRAVIS" = xtrue ]; then
 	CPU_COUNT=2
 fi
 
-unset CFLAGS
-unset CXXFLAGS
-unset CPPFLAGS
-unset DEBUG_CXXFLAGS
-unset DEBUG_CPPFLAGS
-unset LDFLAGS
-
-INCLUDES="$(PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig pkg-config --dont-define-prefix --cflags libftdi1 libusb libudev)"
-INCLUDES='-I/usr/include/libftdi1 -I/usr/include/libusb-1.0'
-STATIC_LIBS="$(PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig pkg-config --dont-define-prefix --libs libftdi1 libusb)"
-STATIC_LIBS="-L/usr/lib/x86_64-linux-gnu -lftdi1 -lusb-1.0 -lusb"
-DYNAMIC_LIBS="$(PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig pkg-config --dont-define-prefix --libs libudev)"
-DYNAMIC_LIBS="-L/lib/x86_64-linux-gnu -ludev"
-
-export CFLAGS="$CFLAGS $INCLUDES"
-export LDFLAGS="$LDFLAGS -lm -lstdc++ -lpthread -Wl,--whole-archive -Wl,-Bstatic $STATIC_LIBS -Wl,-Bdynamic -Wl,--no-whole-archive $DYNAMIC_LIBS"
+#unset CFLAGS
+#unset CXXFLAGS
+#unset CPPFLAGS
+#unset DEBUG_CXXFLAGS
+#unset DEBUG_CPPFLAGS
+#unset LDFLAGS
+#
+#INCLUDES="$(PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig pkg-config --dont-define-prefix --cflags libftdi1 libusb libudev)"
+#INCLUDES='-I/usr/include/libftdi1 -I/usr/include/libusb-1.0'
+#STATIC_LIBS="$(PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig pkg-config --dont-define-prefix --libs libftdi1 libusb)"
+#STATIC_LIBS="-L/usr/lib/x86_64-linux-gnu -lftdi1 -lusb-1.0 -lusb"
+#DYNAMIC_LIBS="$(PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig pkg-config --dont-define-prefix --libs libudev)"
+#DYNAMIC_LIBS="-L/lib/x86_64-linux-gnu -ludev"
+#
+#export CFLAGS="$CFLAGS $INCLUDES"
+#export LDFLAGS="$LDFLAGS -lm -lstdc++ -lpthread -Wl,--whole-archive -Wl,-Bstatic $STATIC_LIBS -Wl,-Bdynamic -Wl,--no-whole-archive $DYNAMIC_LIBS"
 
 make -j$CPU_COUNT
 
 make install
 
-iceprog -h
+iceprog --help
--- libftdi/build.sh
+++ libftdi/build.sh
@@ -5,6 +5,21 @@ cd build
 if [ $(uname -m) = "i686" ]; then
   COMPILER32="-DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32"
 fi
-cmake .. $COMPILER32 -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_PREFIX_PATH=$PREFIX
+export PKG_CONFIG_PATH=$BUILD_PREFIX/lib/pkgconfig
+
+echo "--------------------------"
+which pkg-config
+echo "--------------------------"
+pkg-config --debug --libs libusb-1.0
+echo "--------------------------"
+
+cmake .. 				\
+	$COMPILER32 			\
+	-Wno-dev 			\
+      	-DCMAKE_BUILD_TYPE=Release      \
+	-DCMAKE_INSTALL_PREFIX=$PREFIX 	\
+	-DCMAKE_PREFIX_PATH=$PREFIX 	\
+	-DBOOST_ROOT=${PREFIX}          \
+
 make -j2
 make install
