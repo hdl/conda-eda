549fd365bec9e592023f8091887bb6ca449e9c81
Tim 'mithro' Ansell <me@mith.ro>
2019-05-04 10:35:22 -0700

    openocd: Use libftdi / libusb from conda.
    As we now have libftdi / libusb in conda packages, link against them
    rather then trying to statically link.

--- openocd/build.sh
+++ openocd/build.sh
@@ -5,15 +5,9 @@ set -x
 
 if [ x"$TRAVIS" = xtrue ]; then
 	CPU_COUNT=2
+else
+	CPU_COUNT=$(nproc)
 fi
-
-#export GIT_AUTHOR_NAME="Conda Build"
-#export GIT_AUTHOR_EMAIL="robot@timvideos.us"
-#export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
-#export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
-#git commit -a -m "Changes for conda."
-
-git status
 
 ./bootstrap
 mkdir build
@@ -29,19 +23,4 @@ cd build
 
 
 make -j$CPU_COUNT
-
-echo "---------------------------"
-ldd ./src/openocd
-ls -l ./src/openocd
-du -h ./src/openocd
-echo "---------------------------"
-gcc -Wall -Wstrict-prototypes -Wformat-security -Wshadow -Wextra -Wno-unused-parameter -Wbad-function-cast -Wcast-align -Wredundant-decls -Werror -g -O2 -o src/openocd src/main.o -Wl,-Bstatic src/.libs/libopenocd.a ./jimtcl/libjim.a -lusb-1.0 -lusb -lftdi -Wl,-Bdynamic -Wl,--no-whole-archive -ludev -lpthread -ldl -lm
-ldd ./src/openocd
-ls -l ./src/openocd
-du -h ./src/openocd
-echo "---------------------------"
-strip ./src/openocd
-ls -l ./src/openocd
-du -h ./src/openocd
-
 make install
