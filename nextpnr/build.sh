1bd8c00f5a1bc37f163c97256298e3f583b64a97
Tim 'mithro' Ansell <me@mith.ro>
2019-05-03 23:30:53 -0700

    nextpnr: Only use 1 CPU.

--- nextpnr/build.sh
+++ nextpnr/build.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 if [ x"$TRAVIS" = xtrue ]; then
-	CPU_COUNT=2
+	CPU_COUNT=1
 fi
 
 cmake -DARCH=ice40 -DBUILD_GUI=OFF -DICEBOX_ROOT=${PREFIX}/share/icebox -DCMAKE_INSTALL_PREFIX=/ -DENABLE_READLINE=No .
