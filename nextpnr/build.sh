26f174a6bd37619b0b155a0f8f6fd43c34c4fd9a
Tim 'mithro' Ansell <me@mith.ro>
2019-05-09 08:31:33 -0700

    nextpnr: Try make a few more times.

--- nextpnr/build.sh
+++ nextpnr/build.sh
@@ -4,11 +4,14 @@ set -e
 set -x
 
 if [ x"$TRAVIS" = xtrue ]; then
-	CPU_COUNT=1
+	CPU_COUNT=2
+	cat /proc/meminfo
 else
 	CPU_COUNT=$(nproc)
 fi
 
 cmake -DARCH=ice40 -DBUILD_GUI=OFF -DICEBOX_ROOT=${PREFIX}/share/icebox -DCMAKE_INSTALL_PREFIX=/ -DENABLE_READLINE=No .
-make -j${CPU_COUNT}
+make -k -j${CPU_COUNT} || true
+make
+
 make DESTDIR=${PREFIX} install
