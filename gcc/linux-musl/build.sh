800544240e14c2058aff446c9a2c32b74fb30536
Tim 'mithro' Ansell <me@mith.ro>
2019-07-28 13:25:10 -0400

    gcc/linux-musl: Use full 2 stage GCC build.
     * Build binutils
     * Build gcc-stage 1 (no libc)
     * Install Linux Kernel headers
     * Build and Install musl
     * Build gcc-stage 2 with musl as libc

--- gcc/linux-musl/build.sh
+++ gcc/linux-musl/build.sh
@@ -62,50 +62,7 @@ echo "============================================================"
 echo
 echo
 
-METAL_TARGET=${TOOLCHAIN_ARCH}-elf
 LINUX_TARGET=${TOOLCHAIN_ARCH}-linux-musl
-
-# ============================================================
-
-# Check the "nostdc" gcc is already installed
-echo -n "---?"
-which $METAL_TARGET-gcc
-ls -l $(which $METAL_TARGET-gcc)
-file $(which $METAL_TARGET-gcc)
-echo "---"
-$METAL_TARGET-gcc --version 2>&1
-echo "---"
-
-GCC_STAGE1_VERSION=$($METAL_TARGET-gcc --version 2>&1 | head -1 | sed -e"s/$METAL_TARGET-gcc (//" -e"s/).*//")
-GCC_STAGE2_VERSION=$(echo $PKG_VERSION | sed -e's/-.*//')
-if [ "$GCC_STAGE1_VERSION" != "$GCC_STAGE2_VERSION" ]; then
-	echo
-	echo "nostdc version: $GCC_STAGE1_VERSION"
-	echo "  this version: $GCC_STAGE2_VERSION"
-	echo
-	echo "Stage 1 compiler (nostdc) not the same version as us!"
-	echo
- 	exit 1
-fi
-
-cd ..
-
-# ============================================================
-
-case "${TOOLCHAIN_ARCH}" in
-	riscv*)	LINUX_ARCH=riscv 	;;
-	or1k*)	LINUX_ARCH=openrisc	;;
-	sh*)	LINUX_ARCH=sh		;;
-esac
-export LINUX_ARCH
-
-cd $SRC_DIR/kernel-headers
-make defconfig ARCH=$LINUX_ARCH
-mkdir -p $PREFIX/$LINUX_TARGET/include
-mkdir -p $PREFIX/$LINUX_TARGET/usr
-ln -s $PREFIX/$LINUX_TARGET/include $PREFIX/$LINUX_TARGET/usr/include
-make headers_install ARCH=$LINUX_ARCH INSTALL_HDR_PATH=$PREFIX/$LINUX_TARGET/include
-find $PREFIX/$LINUX_TARGET | sort
 
 # ============================================================
 
@@ -144,50 +101,190 @@ echo "---"
 
 # ============================================================
 
-# Configure GCC
-mkdir -p $SRC_DIR/build-gcc
-cd $SRC_DIR/build-gcc
-$SRC_DIR/gcc/configure \
-	\
-        --prefix=/ \
-	--with-sysroot=$PREFIX/$LINUX_TARGET \
-	\
-	--program-prefix=$LINUX_TARGET- \
-	\
-        --with-gmp=$CONDA_PREFIX \
-        --with-mpfr=$CONDA_PREFIX \
-        --with-mpc=$CONDA_PREFIX \
-        --with-isl=$CONDA_PREFIX \
-        --with-cloog=$CONDA_PREFIX \
-	\
-	--target=$LINUX_TARGET \
-	--with-pkgversion=$PKG_VERSION \
-	--enable-languages="c" \
-	--enable-multilib \
-	\
-	--with-musl \
-	\
-	--disable-nls \
-	--disable-libatomic \
-	--enable-libgcc \
-	--disable-libgomp \
-	--disable-libmudflap \
-	--disable-libquadmath \
-	--disable-libssp \
-	--disable-nls \
-	--disable-shared \
-	--disable-tls \
-	\
+SYSROOT=$PREFIX/$TARGET/sysroot
+mkdir -p $SYSROOT/usr/include
 
-# Build GCC
-mkdir -p $SRC_DIR/build-gcc
-cd $SRC_DIR/build-gcc
+# ============================================================
 
-make -j$CPU_COUNT
-make DESTDIR=${PREFIX} install-strip
+(
+	# Stage 1 GCC
+	export CFLAGS="$CFLAGS -O0 -g0"
+	export CXXFLAGS="$CXXFLAGS -O0 -g0"
+
+	mkdir -p $SRC_DIR/build-gcc1
+	cd $SRC_DIR/build-gcc1
+	$SRC_DIR/gcc1/configure \
+		\
+		--prefix=/ \
+		--with-sysroot=$SYSROOT \
+		\
+		--target=$LINUX_TARGET \
+		--with-pkgversion=$PKG_VERSION \
+		--enable-languages="c" \
+		--enable-threads=single \
+		--enable-multilib \
+		\
+		--without-headers \
+		\
+		--disable-nls \
+		--disable-libatomic \
+		--disable-libgcc \
+		--disable-libgomp \
+		--disable-libmudflap \
+		--disable-libquadmath \
+		--disable-libssp \
+		--disable-nls \
+		--disable-shared \
+		--disable-tls \
+		\
+
+	# Build GCC
+	mkdir -p $SRC_DIR/build-gcc1
+	cd $SRC_DIR/build-gcc1
+
+	make -j$CPU_COUNT
+	make DESTDIR=${PREFIX} install-strip
+
+	# Check GCC
+	cd $SRC_DIR
+	echo -n "---?"
+	which $LINUX_TARGET-gcc
+	ls -l $(which $LINUX_TARGET-gcc)
+	file $(which $LINUX_TARGET-gcc)
+	echo "---"
+	$LINUX_TARGET-gcc --version 2>&1
+	echo "---"
+)
+
+# ============================================================
+
+(
+	# Install C library
+	unset AS
+	unset AR
+	unset ADDR2LINE
+	unset CC
+	unset CFLAGS
+	unset CPP
+	unset CPPFLAGS
+	unset DEBUG_CPPFLAGS
+	unset CXX
+	unset CXXFLAGS
+	unset DEBUG_CXXFLAGS
+	unset GCC
+	unset GCC_AR
+	unset GCC_NM
+	unset GCC_RANLIB
+	unset GPROF
+	unset GXX
+	unset LD
+	unset LDFLAGS
+	unset LD_GOLD
+	unset LD_RUN_PATH
+	unset RANLIB
+	unset READELF
+	unset SIZE
+	unset STRINGS
+	unset STRIP
+
+	export PATH=${PREFIX}/bin:$PATH
+	# GCC_EXEC_PREFIX
+	# COMPILER_PATH
+
+	echo "========================================================="
+	$PREFIX/bin/$LINUX_TARGET-gcc --version
+	$PREFIX/bin/$LINUX_TARGET-gcc -print-search-dirs
+	$PREFIX/bin/$LINUX_TARGET-gcc -print-prog-name=as
+	echo "========================================================="
+
+	export CROSS_COMPILE="$LINUX_TARGET-"
+	export CC="$LINUX_TARGET-gcc"
+
+	# Linux Headers
+	case "${TOOLCHAIN_ARCH}" in
+		riscv*)	LINUX_ARCH=riscv 	;;
+		or1k*)	LINUX_ARCH=openrisc	;;
+		sh*)	LINUX_ARCH=sh		;;
+	esac
+	export LINUX_ARCH
+
+	cd $SRC_DIR/kernel-headers
+	make defconfig ARCH=$LINUX_ARCH
+	make headers_install ARCH=$LINUX_ARCH INSTALL_HDR_PATH=$SYSROOT/usr/include
+	find $SYSROOT | sort
+	ls -ld $SYSROOT/include
+	ls -ld $SYSROOT/usr/include
+
+	# Configure musl
+	mkdir -p $SRC_DIR/build-musl
+	cd $SRC_DIR/build-musl
+	$SRC_DIR/musl/configure \
+		\
+		--target=$LINUX_TARGET \
+		\
+		--prefix=/ \
+		--with-sysroot=$SYSROOT \
+		\
+		--enable-multilib \
+		\
+
+	# Build libc (musl)
+	mkdir -p $SRC_DIR/build-musl
+	cd $SRC_DIR/build-musl
+	make -j$CPU_COUNT
+	make DESTDIR=$PREFIX install
+
+)
+
+# ============================================================
+
+(
+	# Stage 2 GCC
+	mkdir -p $SRC_DIR/build-gcc2
+	cd $SRC_DIR/build-gcc2
+
+	# Configure GCC
+	$SRC_DIR/gcc/configure \
+		\
+		--prefix=/ \
+		--with-sysroot=$PREFIX/$LINUX_TARGET \
+		\
+		--program-prefix=$LINUX_TARGET- \
+		\
+		--with-gmp=$CONDA_PREFIX \
+		--with-mpfr=$CONDA_PREFIX \
+		--with-mpc=$CONDA_PREFIX \
+		--with-isl=$CONDA_PREFIX \
+		--with-cloog=$CONDA_PREFIX \
+		\
+		--target=$LINUX_TARGET \
+		--with-pkgversion=$PKG_VERSION \
+		--enable-languages="c" \
+		--enable-multilib \
+		\
+		--with-musl \
+		\
+		--disable-nls \
+		--disable-libatomic \
+		--enable-libgcc \
+		--disable-libgomp \
+		--disable-libmudflap \
+		--disable-libquadmath \
+		--disable-libssp \
+		--disable-nls \
+		--disable-shared \
+		--disable-tls \
+		\
+
+	# Build GCC
+	mkdir -p $SRC_DIR/build-gcc2
+	cd $SRC_DIR/build-gcc2
+
+	make -j$CPU_COUNT
+	make DESTDIR=${PREFIX} install-strip
+)
 
 # Check GCC
-cd $SRC_DIR
 echo -n "---?"
 which $LINUX_TARGET-gcc
 ls -l $(which $LINUX_TARGET-gcc)
@@ -196,70 +293,6 @@ echo "---"
 $LINUX_TARGET-gcc --version 2>&1
 echo "---"
 
-# ============================================================
-
-unset AS
-unset AR
-unset ADDR2LINE
-unset CC
-unset CFLAGS
-unset CPP
-unset CPPFLAGS
-unset DEBUG_CPPFLAGS
-unset CXX
-unset CXXFLAGS
-unset DEBUG_CXXFLAGS
-unset GCC
-unset GCC_AR
-unset GCC_NM
-unset GCC_RANLIB
-unset GPROF
-unset GXX
-unset LD
-unset LDFLAGS
-unset LD_GOLD
-unset LD_RUN_PATH
-unset RANLIB
-unset READELF
-unset SIZE
-unset STRINGS
-unset STRIP
-
-export PATH=${PREFIX}/bin:$PATH
-# GCC_EXEC_PREFIX
-# COMPILER_PATH
-
-echo "========================================================="
-$PREFIX/bin/$LINUX_TARGET-gcc --version
-$PREFIX/bin/$LINUX_TARGET-gcc -print-search-dirs
-$PREFIX/bin/$LINUX_TARGET-gcc -print-prog-name=as
-echo "========================================================="
-
-export CROSS_COMPILE="$LINUX_TARGET-"
-export CC="$LINUX_TARGET-gcc"
-
-# ============================================================
-
-# Configure musl
-mkdir -p $SRC_DIR/build-musl
-cd $SRC_DIR/build-musl
-$SRC_DIR/musl/configure \
-	\
-	--target=$LINUX_TARGET \
-	\
-        --prefix=/ \
-	--with-sysroot=$PREFIX/$LINUX_TARGET \
-	\
-	--enable-multilib \
-	\
-
-# Build libc (musl)
-mkdir -p $SRC_DIR/build-musl
-cd $SRC_DIR/build-musl
-make -j$CPU_COUNT
-make DESTDIR=$PREFIX install
-
-# ============================================================
 
 $PREFIX/bin/$METAL_TARGET-gcc --version
 $PREFIX/bin/$LINUX_TARGET-gcc --version
