e0702ff4f5f8248168aadc44b87833fdb5dc94eb
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2019-12-01 03:29:31 +0100

    Fix SDK downloading

--- .travis/download_sdk.sh
+++ .travis/download_sdk.sh
@@ -1,3 +1,5 @@
-if [[ ! -d $HOME/sdk ]]; then
-    git clone https://github.com/phracker/MacOSX-SDKs $HOME/sdk
+if [ $TRAVIS_OS_NAME = 'osx' ]; then
+    if [[ ! -d $HOME/sdk/MacOSX10.9.sdk ]]; then
+        git clone https://github.com/phracker/MacOSX-SDKs $HOME/sdk
+    fi
 fi
