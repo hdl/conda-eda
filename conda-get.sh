5667c46d6ffab6b97139bdcf2506f8028860eb50
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2019-12-01 03:02:25 +0100

    Add macOS support to conda-get

--- conda-get.sh
+++ conda-get.sh
@@ -5,10 +5,16 @@ set -e
 
 CONDA_PATH=${1:-~/conda}
 
-wget -c https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-chmod a+x Miniconda3-latest-Linux-x86_64.sh
+if [ $TRAVIS_OS_NAME = 'linux' ]; then
+    sys_name=Linux
+else
+    sys_name=MacOSX
+fi
+
+wget -c https://repo.continuum.io/miniconda/Miniconda3-latest-${sys_name}-x86_64.sh
+chmod a+x Miniconda3-latest-${sys_name}-x86_64.sh
 if [ ! -d $CONDA_PATH -o ! -z "$CI"  ]; then
-        ./Miniconda3-latest-Linux-x86_64.sh -p $CONDA_PATH -b -f
+        ./Miniconda3-latest-${sys_name}-x86_64.sh -p $CONDA_PATH -b -f
 fi
 export PATH=$CONDA_PATH/bin:$PATH
 
