5d62b35084c12577631f50a34114257390884d57
Jan Kowalewski <jkowalewski@antmicro.com>
2019-12-04 12:32:42 +0100

    Handle setting jobs in OSX and add -Wno-error

--- prog/openocd/build.sh
+++ prog/openocd/build.sh
@@ -6,7 +6,20 @@ set -x
 if [ x"$TRAVIS" = xtrue ]; then
 	CPU_COUNT=2
 else
-	CPU_COUNT=$(nproc)
+	# Identify OS
+	UNAME_OUT="$(uname -s)"
+	case "${UNAME_OUT}" in
+		Linux*)     OS=Linux;;
+		Darwin*)    OS=Mac;;
+		*)          OS="${UNAME_OUT}"
+		            echo "Unknown OS: ${OS}"
+		            exit 1;;
+	esac
+	if [[ $OS == "Linux" ]]; then
+		CPU_COUNT=$(nproc)
+	elif [[ $OS == "Mac" ]]; then
+		CPU_COUNT=$(sysctl -n hw.physicalcpu)
+	fi
 fi
 
 mv tcl/target/1986ве1т.cfg tcl/target/1986be1t.cfg
@@ -23,6 +36,7 @@ cd build
   --enable-usb_blaster_libftdi \
   --enable-jtag_vpi \
   --enable-remote-bitbang \
+  CFLAGS="-Wno-error"
 
 
 make -j$CPU_COUNT
