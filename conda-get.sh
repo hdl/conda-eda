6eee510e8119fbd5312c74882b6a46f522d7fe6f
Jakub Jatczak <jjatczak@antmicro.com>
2019-12-19 15:52:33 +0100

    Add windows support to conda-get

--- conda-get.sh
+++ conda-get.sh
@@ -4,19 +4,29 @@ set -x
 set -e
 
 CONDA_PATH=${1:-~/conda}
-
-if [ $TRAVIS_OS_NAME = 'linux' ]; then
-    sys_name=Linux
+if [ $TRAVIS_OS_NAME = 'windows' ]; then
+    if [ ! -d $CONDA_PATH -o ! -z "$CI"  ]; then
+        choco install openssl.light
+        choco install miniconda3 --params="/AddToPath:1"
+    fi
+    export CONDA_PATH='/c/tools/miniconda3'
+    export PATH=$CONDA_PATH/bin/:$CONDA_PATH/Scripts/:$PATH
 else
-    sys_name=MacOSX
+    if [ $TRAVIS_OS_NAME = 'linux' ]; then
+        sys_name=Linux
+    else
+        sys_name=MacOSX
+    fi
+
+    wget -c https://repo.continuum.io/miniconda/Miniconda3-latest-${sys_name}-x86_64.sh
+    chmod a+x Miniconda3-latest-${sys_name}-x86_64.sh
+    if [ ! -d $CONDA_PATH -o ! -z "$CI"  ]; then
+            ./Miniconda3-latest-${sys_name}-x86_64.sh -p $CONDA_PATH -b -f
+    fi
+    export PATH=$CONDA_PATH/bin:$PATH
 fi
 
-wget -c https://repo.continuum.io/miniconda/Miniconda3-latest-${sys_name}-x86_64.sh
-chmod a+x Miniconda3-latest-${sys_name}-x86_64.sh
-if [ ! -d $CONDA_PATH -o ! -z "$CI"  ]; then
-        ./Miniconda3-latest-${sys_name}-x86_64.sh -p $CONDA_PATH -b -f
-fi
-export PATH=$CONDA_PATH/bin:$PATH
+echo $PATH
 
 conda info
 
@@ -35,7 +45,10 @@ conda update -y conda
 
 conda install -y conda-build
 conda install -y conda-verify
-conda install -y ripgrep
+
+if [ $TRAVIS_OS_NAME != 'windows' ]; then
+    conda install -y ripgrep
+fi
 
 conda install -y anaconda-client
 conda install -y jinja2
