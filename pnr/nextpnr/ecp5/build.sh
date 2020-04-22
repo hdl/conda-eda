bc485b8fef01f13d3a03a873f564b93580899093
Jan Kowalewski <jkowalewski@antmicro.com>
2020-04-22 16:01:01 +0200

    pnr/nextpnr/ecp5: fix linux target

--- pnr/nextpnr/ecp5/build.sh
+++ pnr/nextpnr/ecp5/build.sh
@@ -5,13 +5,16 @@ set -x
 
 if [ x"$TRAVIS" = xtrue ]; then
 	CPU_COUNT=2
-	cat /proc/meminfo
-else
-	CPU_COUNT=$(nproc)
 fi
 
-cmake -DARCH=ecp5 -DBUILD_GUI=OFF -DTRELLIS_ROOT=${PREFIX}/share/trellis -DCMAKE_INSTALL_PREFIX=/ -DENABLE_READLINE=No .
-make -k -j${CPU_COUNT} || true
-make
+cmake \
+	-DARCH=ecp5				\
+	-DBUILD_GUI=OFF				\
+	-DTRELLIS_INSTALL_PREFIX=${PREFIX}	\
+	-DTRELLIS_ROOT=${PREFIX}/share/trellis	\
+	-DCMAKE_INSTALL_PREFIX=/		\
+	-DENABLE_READLINE=No			\
+	.
 
+make -k -j${CPU_COUNT} || true
 make DESTDIR=${PREFIX} install
