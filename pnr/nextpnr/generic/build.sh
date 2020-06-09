6d79715adeeca15565bd5d4173cfacfac7210c8f
Jakub Jatczak <jjatczak@antmicro.com>
2020-06-09 14:37:25 +0200

    pnr/nextpnr/generic: add OS X build

--- pnr/nextpnr/generic/build.sh
+++ pnr/nextpnr/generic/build.sh
@@ -4,10 +4,23 @@ set -e
 set -x
 
 if [ x"$TRAVIS" = xtrue ]; then
-	CPU_COUNT=2
-	cat /proc/meminfo
+    CPU_COUNT=2
 else
-	CPU_COUNT=$(nproc)
+    # Identify OS
+    UNAME_OUT="$(uname -s)"
+    case "${UNAME_OUT}" in
+    Linux*)     OS=Linux;;
+    Darwin*)    OS=Mac;;
+    *)          OS="${UNAME_OUT}"
+                echo "Unknown OS: ${OS}"
+                exit;;
+    esac
+
+    if [[ $OS == "Linux" ]]; then
+        CPU_COUNT=$(nproc)
+    elif [[ $OS == "Mac" ]]; then
+        CPU_COUNT=$(sysctl -n hw.physicalcpu)
+    fi
 fi
 
 cmake -DARCH=generic -DBUILD_GUI=OFF -DCMAKE_INSTALL_PREFIX=/ -DENABLE_READLINE=No .
