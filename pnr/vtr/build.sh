5bba7ed925a845543e30c5354774d2c94442d442
Jan Kowalewski <43608048+kowalewskijan@users.noreply.github.com>
2020-06-10 19:02:53 +0200

    pnr/vtr: add Linux and OSX targets (#16)

--- pnr/vtr/build.sh
+++ pnr/vtr/build.sh
@@ -7,9 +7,20 @@ if [ x"$TRAVIS" = xtrue ]; then
 	CPU_COUNT=2
 fi
 
+# Identify OS
+UNAME_OUT="$(uname -s)"
+case "${UNAME_OUT}" in
+    Linux*)     OS=Linux;;
+    Darwin*)    OS=Mac;;
+    *)          OS="${UNAME_OUT}"
+                echo "Unknown OS: ${OS}"
+                exit;;
+esac
 
-make V=1 CMAKE_PARAMS="-DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON" -j$CPU_COUNT
-make test
+make V=1 CMAKE_PARAMS="-DCMAKE_INSTALL_PREFIX=${PREFIX}" -j$CPU_COUNT
+if [[ $OS != "Mac" ]]; then
+	make test
+fi
 make install
 
 mkdir -p ${PREFIX}/lib
