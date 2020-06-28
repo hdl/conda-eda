ec6316c86643d7a51239df401f2d47abc2046355
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2020-06-28 13:27:10 +0200

    Add Magic package for Linux

---
+++ misc/magic/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [ x"$TRAVIS" = xtrue ]; then
+	CPU_COUNT=2
+fi
+
+# Identify OS
+UNAME_OUT="$(uname -s)"
+case "${UNAME_OUT}" in
+    Linux*)     OS=Linux;;
+    *)          OS="${UNAME_OUT}"
+                echo "Unknown OS: ${OS}"
+                exit;;
+esac
+
+tcsh=`which tcsh`
+ln -s $tcsh $(dirname $tcsh)/csh
+
+./configure --prefix="${PREFIX}" --with-cairo="${BUILD_PREFIX}/include"
+
+make V=1 -j$CPU_COUNT
+
+make V=1 install
