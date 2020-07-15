f9f425bd13e72dcef9bfbfe25bad5f1274e958bb
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2020-07-15 16:18:42 +0200

    Rework package uploading to handle temporary labels

--- after_success.sh
+++ after_success.sh
@@ -11,14 +11,14 @@ if [[ $UPLOAD == "no-upload" ]]; then
 else
     echo "Job with Conda upload..."
 
-    if [ x$TRAVIS_BRANCH = x"master" -a x$TRAVIS_EVENT_TYPE != x"cron" -a x$TRAVIS_PULL_REQUEST == xfalse ]; then
-    	$SPACER
-
-    	start_section "package.upload" "${GREEN}Package uploading...${NC}"
-    	anaconda -t $ANACONDA_TOKEN upload --user $ANACONDA_USER --label main $CONDA_OUT
-    	end_section "package.upload"
+    $SPACER
+    # Travis will not expose the ANACONDA_TOKEN var for pull requests coming from other forks than the original one
+    if [ x$ANACONDA_TOKEN != x ]; then
+        start_section "package.upload" "${GREEN}Package uploading...${NC}"
+        anaconda -t $ANACONDA_TOKEN upload --no-progress --user $ANACONDA_USER --label travis-$TRAVIS_BUILD_ID $CONDA_OUT
+        end_section "package.upload"
     else
-        echo "Conditions unmet, will not upload"
+        echo "Cannot upload packages from branches not uploaded to the original repository"
     fi
 
     $SPACER
