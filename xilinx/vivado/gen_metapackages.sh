dc850f88b331918f87e17cc589a043eb967d896f
Adam Jeliński <ajelinski@antmicro.com>
2020-12-01 12:16:31 +0100

    xilinx-vivado: Adapt script to work with conda-build-prepare

--- xilinx/vivado/gen_metapackages.sh
+++ xilinx/vivado/gen_metapackages.sh
@@ -4,6 +4,20 @@ input=$VERSIONS
 while IFS= read -r version
 do
 	export XILINX_VIVADO_VERSION=$version
+
+	# Initialize Conda
+	. $TRAVIS_BUILD_DIR/.travis/common.sh
+	eval "$('conda' 'shell.bash' 'hook' 2> /dev/null)"
+
+	# Remove previously prepared recipe directory
+	rm -rf workdir
+
+	# Prepare recipe with currently set version (cbp is installed in `base` environment)
+	conda activate base
+	ADDITIONAL_PACKAGES="anaconda-client conda-build=3.20.3 conda-verify jinja2 pexpect python=3.7 ripgrep"
+	python -m conda_build_prepare --dir workdir --packages $ADDITIONAL_PACKAGES -- $PACKAGE
+
+	# Build metapackage
 	bash $TRAVIS_BUILD_DIR/.travis/script.sh
 	bash $TRAVIS_BUILD_DIR/.travis/after_success.sh
 done < "$input"
