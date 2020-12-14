8625658262cc74270a9cadab276d67b10b0e1b7b
Dusty DeWeese <dustin.deweese@gmail.com>
2020-12-14 14:44:26 -0800

    Add comments.
    Signed-off-by: Dusty DeWeese <dustin.deweese@gmail.com>

--- pnr/vtr-optimized/run-sf.sh
+++ pnr/vtr-optimized/run-sf.sh
@@ -23,8 +23,21 @@ quiet symbiflow_pack -e ${EBLIF} -d ${DEVICE} -s ${SDC}
 
 # Place
 # This should be:
-#   quiet symbiflow_place -e ${EBLIF} -d ${DEVICE} -n ${NET}.net -P ${PART} -s ${SDC}
-# But the following lines are needed to override the placement constraints
+#
+#     quiet symbiflow_place -e ${EBLIF} -d ${DEVICE} -n ${NET}.net -P ${PART} -s ${SDC}
+#
+# Currently, using this command results in:
+#
+#     Traceback (most recent call last):
+#       File ".../symbiflow/share/symbiflow/scripts/prjxray_create_ioplace.py", line 178, in <module>
+#         main()
+#       File ".../symbiflow/share/symbiflow/scripts/prjxray_create_ioplace.py", line 169, in main
+#         io_place.output_io_place(args.output)
+#       File ".../symbiflow/share/symbiflow/scripts/vpr_io_place.py", line 151, in output_io_place
+#         max_name_length = max(len(c.name) for c in self.constraints.values())
+#     ValueError: max() arg is an empty sequence
+#
+# The following lines are needed to override the placement constraints
 # with those that were downloaded instead of the generated ones.
 (
     export MYPATH=${BUILD_ROOT}/symbiflow/bin
