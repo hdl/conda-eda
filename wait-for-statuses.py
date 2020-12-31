8c7d40fd6c31cc8cbf1ea2881295aee4ffd56956
Tomasz Jurtsch <tjurtsch@antmicro.com>
2020-12-31 10:04:21 +0100

    Ensure that all the other jobs were completed before
    We use this simple counter condition to determine if
    all jobs were queued and completed. Not queued jobs
    aren't returned in API call.

--- wait-for-statuses.py
+++ wait-for-statuses.py
@@ -10,15 +10,20 @@ status_url = "https://api.github.com/repos/" \
          + os.environ['GITHUB_RUN_ID'] \
          + "/jobs"
 
+numOfJobs = int(os.environ['NUM_OF_JOBS'])
+
 while(True):
   time.sleep(60)
-  success = True
+  countCompleted = 0
+
   with urllib.request.urlopen(status_url) as url:
     data = json.loads(url.read().decode())
     for j in data["jobs"]:
-      if(j["status"] != "completed" and j["name"] != "master-package"):
-        success = False
-  if(success):
+      if(j["status"] == "completed"):
+        countCompleted += 1
+
+  print("Completed jobs:" + str(countCompleted) + ". Jobs overall: " + str(numOfJobs))
+  if(countCompleted >= numOfJobs):
     break
 
 subprocess.call(os.environ['GITHUB_WORKSPACE'] + "/.github/scripts/master-package.sh")
