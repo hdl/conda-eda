ce25d236f590d242de5cb62ae9b1f26c5613f40e
Tomasz Jurtsch <tjurtsch@antmicro.com>
2021-01-05 10:22:40 +0100

    test_anaconda: don't exit when token works
    This caused upload scripts to exit prematurely
    https://github.com/litex-hub/litex-conda-misc/runs/1648904572?check_suite_focus=true

--- test_anaconda.sh
+++ test_anaconda.sh
@@ -6,19 +6,18 @@ if [ x$ANACONDA_TOKEN != x ]; then
         echo 'ERROR: Missing `anaconda-client` package!'
         echo 'This Conda environment contains the following packages:'
         conda list
+        exit 1
     else
         echo 'Testing ANACONDA_TOKEN with a simple `anaconda` call...'
         if anaconda -t $ANACONDA_TOKEN label --show main &>/dev/null; then
             echo 'ANACONDA_TOKEN works!'
-            exit 0
         else
             echo 'ERROR: Invalid ANACONDA_TOKEN!'
+            exit 1
         fi
     fi
 else
     echo 'ERROR: ANACONDA_TOKEN not set!'
+    exit 1
 fi
 echo
-
-# We only get here when anaconda/token doesn't work
-exit 1
