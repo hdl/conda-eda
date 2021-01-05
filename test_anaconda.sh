23289cf9c0b8a497bd92691b381d242e77c22cdc
Tomasz Jurtsch <tjurtsch@antmicro.com>
2021-01-05 09:58:18 +0100

    Return error when anaconda/token doesn't work

--- test_anaconda.sh
+++ test_anaconda.sh
@@ -3,18 +3,22 @@
 echo
 if [ x$ANACONDA_TOKEN != x ]; then
     if ! anaconda -h &>/dev/null; then
-        echo 'WARNING: Missing `anaconda-client` package!'
+        echo 'ERROR: Missing `anaconda-client` package!'
         echo 'This Conda environment contains the following packages:'
         conda list
     else
         echo 'Testing ANACONDA_TOKEN with a simple `anaconda` call...'
         if anaconda -t $ANACONDA_TOKEN label --show main &>/dev/null; then
             echo 'ANACONDA_TOKEN works!'
+            exit 0
         else
-            echo 'WARNING: Invalid ANACONDA_TOKEN!'
+            echo 'ERROR: Invalid ANACONDA_TOKEN!'
         fi
     fi
 else
-    echo 'WARNING: ANACONDA_TOKEN not set!'
+    echo 'ERROR: ANACONDA_TOKEN not set!'
 fi
 echo
+
+# We only get here when anaconda/token doesn't work
+exit 1
