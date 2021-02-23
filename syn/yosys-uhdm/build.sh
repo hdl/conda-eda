c0b2e29a1470c833aff21acd901b73c18af66e27
Adam Jeliński <ajelinski@antmicro.com>
2021-02-23 21:23:42 +0100

    Test

--- syn/yosys-uhdm/build.sh
+++ syn/yosys-uhdm/build.sh
@@ -3,12 +3,6 @@
 set -e
 set -x
 
-echo "======================"
-echo "PYTHONHOME: $PYTHONHOME"
-echo "======================"
-echo "PYTHONPATH: $PYTHONPATH"
-echo "======================"
-
 export PKG_CONFIG_PATH="$BUILD_PREFIX/lib/pkgconfig/"
 export CXXFLAGS="$CXXFLAGS -I$BUILD_PREFIX/include"
 export LDFLAGS="$CXXFLAGS -L$BUILD_PREFIX/lib -lrt -ltinfo"
