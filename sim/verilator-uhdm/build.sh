acbc77d7cdc4a502b49e03bfac1c2bc59a77d244
Paweł Czarnecki <pczarnecki@antmicro.com>
2021-03-08 15:33:13 +0100

    verilator-uhdm: build package with whole 'image' directory contents
    Signed-off-by: Paweł Czarnecki <pczarnecki@antmicro.com>

--- sim/verilator-uhdm/build.sh
+++ sim/verilator-uhdm/build.sh
@@ -18,6 +18,7 @@ mkdir -p "$PREFIX/bin"
 sed -i 's/"verilator_bin"/"verilator_bin-uhdm"/g' image/bin/verilator
 sed -i 's/"verilator_bin_dbg"/"verilator_bin_dbg-uhdm"/g' image/bin/verilator
 
-cp "$SRC_DIR/image/bin/verilator" "$PREFIX/bin/verilator-uhdm"
-cp "$SRC_DIR/image/bin/verilator_bin" "$PREFIX/bin/verilator_bin-uhdm"
-cp "$SRC_DIR/image/bin/verilator_bin_dbg" "$PREFIX/bin/verilator_bin_dbg-uhdm"
+cp -r $SRC_DIR/image/* "$PREFIX/"
+mv "$PREFIX/bin/verilator" "$PREFIX/bin/verilator-uhdm"
+mv "$PREFIX/bin/verilator_bin" "$PREFIX/bin/verilator_bin-uhdm"
+mv "$PREFIX/bin/verilator_bin_dbg" "$PREFIX/bin/verilator_bin_dbg-uhdm"
