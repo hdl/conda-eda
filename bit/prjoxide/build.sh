56f28e1f2a82a9c8502be80b6fa32c0b26934d31
Alessandro Comodi <acomodi@antmicro.com>
2021-06-23 10:56:51 +0200

    bit: add prjoxide
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

---
+++ bit/prjoxide/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# Using conda packages for capnproto and capnproto-java does
+# not seem to work when compiling prjoxide.
+curl -O https://capnproto.org/capnproto-c++-0.8.0.tar.gz
+tar zxf capnproto-c++-0.8.0.tar.gz
+pushd capnproto-c++-0.8.0
+./configure
+make -j`nproc` check
+sudo make install
+popd
+
+git clone https://github.com/capnproto/capnproto-java.git
+pushd capnproto-java
+make -j`nproc`
+sudo make install
+popd
+
+curl --proto '=https' -sSf https://sh.rustup.rs | sh -s -- -y
+
+cd libprjoxide
+cargo install --path prjoxide --all-features --root $PREFIX
