f58cc20395d6535650c69ab130f61d52de9c7696
Kamil Rakoczy <krakoczy@antmicro.com>
2022-01-10 15:57:32 +0100

    Adapt surelog-uhdm to new version of Surelog
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- sv-front/surelog-uhdm/build.sh
+++ sv-front/surelog-uhdm/build.sh
@@ -6,14 +6,7 @@ set -x
 export PKG_CONFIG_PATH="$BUILD_PREFIX/lib/pkgconfig/"
 export CXXFLAGS="$CXXFLAGS -I$BUILD_PREFIX/include"
 export LDFLAGS="$CXXFLAGS -L$BUILD_PREFIX/lib -lrt -ltinfo"
-export CC=gcc-${USE_SYSTEM_GCC_VERSION}
-export CXX=g++-${USE_SYSTEM_GCC_VERSION}
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$BUILD_PREFIX/lib"
 
-make PREFIX=./image release install -j$CPU_COUNT
+make install -j$CPU_COUNT
 
-mkdir -p "$PREFIX/bin"
-mkdir -p "$PREFIX/lib/surelog/sv"
-
-cp "$SRC_DIR/image/lib/surelog/sv/builtin.sv" "$PREFIX/lib/surelog/sv/builtin.sv"
-cp "$SRC_DIR/image/bin/surelog" "$PREFIX/bin/surelog-uhdm"
+mv "$PREFIX/bin/surelog" "$PREFIX/bin/surelog-uhdm"
