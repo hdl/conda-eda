3b5ac115d09ca1705336ca0af050eafdb0f7b737
Kamil Rakoczy <krakoczy@antmicro.com>
2022-01-11 11:45:43 +0100

    Force usage of https:// instead of git://
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- syn/yosys-uhdm/build.sh
+++ syn/yosys-uhdm/build.sh
@@ -5,6 +5,10 @@ set -x
 
 export LDFLAGS="$LDFLAGS -lrt -ltinfo"
 
+# Github dropped support for unauthorized git: https://github.blog/2021-09-01-improving-git-protocol-security-github/
+# Make sure we always use https:// instead of git://
+git config --global url.https://github.com/.insteadOf git://github.com/
+
 cd Surelog && \
 	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_POSITION_INDEPENDENT_CODE=ON -S . -B build && \
 	cmake --build build -j$(nproc) && \
