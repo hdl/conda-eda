c8d203dca34c4f8f9fb66853bbda89b2e4bd7957
Johan Euphrosine <proppy@google.com>
2022-06-22 15:57:58 +0900

    hls/xls: disable conda-forge
    - remove conda-forge requirements
    - install bazel using bazelisk
    - remove obsolete patches
    - strip python host dependencies and patches (not used by binary rules)
    
    hls/xls: add compiler deps
    
    github/workflows/Build: re-enable xls
    
    hls/xls: remove requirements
    
    hls/xls: add proto_to_dslx_main
    
    hls/xls: re-enable llvm toolchain
    
    hls/xls: build proto_to_dslx_main
    
    hls/xls: remove bazel alias

--- hls/xls/build.sh
+++ hls/xls/build.sh
@@ -2,14 +2,18 @@
 
 set -ex
 
-bazel build -c opt \
+chmod +x $SRC_DIR/bazelisk-linux-amd64
+$SRC_DIR/bazelisk-linux-amd64 build -c opt \
   --extra_toolchains=@llvm_toolchain//:cc-toolchain-x86_64-linux \
   //xls/dslx:interpreter_main \
   //xls/dslx:ir_converter_main \
   //xls/tools:opt_main \
-  //xls/tools:codegen_main
+  //xls/tools:codegen_main \
+  //xls/tools:proto_to_dslx_main
 
+mkdir -p $PREFIX/bin/
 cp bazel-bin/xls/dslx/interpreter_main \
    bazel-bin/xls/dslx/ir_converter_main \
    bazel-bin/xls/tools/opt_main \
-   bazel-bin/xls/tools/codegen_main $PREFIX/bin/
+   bazel-bin/xls/tools/codegen_main \
+   bazel-bin/xls/tools/proto_to_dslx_main $PREFIX/bin/
