ba1e034614d242df1ffc90959f9aeea6b352ed83
Adam Jeliński <ajelinski@antmicro.com>
2022-07-19 09:44:19 +0200

    Revert "yosys-f4pga-plugins: Fix handling Makefile flags"
    This reverts commit 14bbf08faf8a67406fa9e1e46eab73a5e162ea81.
    
    The change caused using a compiler from the host OS instead of the Conda
    one even though CXX etc. are set by `Makefile_*.common`.

--- syn/yosys-plugins-symbiflow/build.sh
+++ syn/yosys-plugins-symbiflow/build.sh
@@ -7,7 +7,4 @@ which pkg-config
 
 echo "PREFIX := $PREFIX" >> Makefile.conf
 
-# These are set properly by Makefile but only if not present.
-unset CXX CXXFLAGS LDLIBS LDFLAGS
-
 make UHDM_INSTALL_DIR=$PREFIX install -j$CPU_COUNT
