9b651bfeac44c27f7acfcca9fc2038534c653dcf
Johan Euphrosine <proppy@google.com>
2022-09-28 00:00:30 +0900

    misc: add openlane
    Co-authored-by: Adam Jeliński <59468166+ajelinski@users.noreply.github.com>

---
+++ misc/openlane/env.tcl
@@ -0,0 +1,22 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# define system variables
+set ::env(OPENLANE_ROOT) "$::env(CONDA_PREFIX)/share/openlane"
+set ::env(OL_INSTALL_DIR) "$::env(CONDA_PREFIX)/share/openlane/install"
+set ::env(OPENLANE_LOCAL_INSTALL) 1
+set ::env(TCLLIBPATH) [glob -type d "$::env(CONDA_PREFIX)/lib/tcllib*"]
+lappend ::auto_path $::env(TCLLIBPATH)
+# default to conda-install PDKs
+set ::env(PDK_ROOT) "$::env(CONDA_PREFIX)/share/pdk"
