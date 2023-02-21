237c0e31b234d93d05e002c42daa21867c93b5c8
Johan Euphrosine <proppy@google.com>
2023-02-21 23:42:34 +0900

    misc/openlane: enable klayout and lvs

--- misc/openlane/disable-missing-tools.tcl
+++ misc/openlane/disable-missing-tools.tcl
@@ -14,8 +14,3 @@
 
 # https://github.com/hdl/conda-eda/issues/174
 set ::env(RUN_CVC) 0
-# https://github.com/The-OpenROAD-Project/OpenLane/issues/1380
-set ::env(RUN_LVS) 0
-# https://github.com/hdl/conda-eda/issues/175
-set ::env(RUN_KLAYOUT) 0
-set ::env(RUN_KLAYOUT_XOR) 0
