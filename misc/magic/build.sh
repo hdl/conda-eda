dfa33a0f1884b4d6bb1cf843a6aca04b68d00e48
Sean Cross <sean@xobs.io>
2023-02-24 10:37:02 +0800

    misc/magic: disable cairo, opengl, and x on osx
    We don't have a full X11 working on osx, so disable anything
    related to X11 when building on that platform.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- misc/magic/build.sh
+++ misc/magic/build.sh
@@ -7,12 +7,17 @@ set -x
 UNAME_OUT="$(uname -s)"
 case "${UNAME_OUT}" in
     Linux*)     OS=Linux;;
+    Darwin*)    OS=Mac;;
     *)          OS="${UNAME_OUT}"
                 echo "Unknown OS: ${OS}"
                 exit;;
 esac
 
-./configure --prefix="${PREFIX}" --with-cairo="${BUILD_PREFIX}/include"
+if [ "$OS" = "Linux" ]; then
+    ./configure --prefix="${PREFIX}" --with-cairo="${BUILD_PREFIX}/include"
+elif [ "$OS" = "Mac" ]; then
+    ./configure --prefix="${PREFIX}" --without-cairo --without-opengl --without-x
+fi
 
 make V=1 -j$CPU_COUNT
 
