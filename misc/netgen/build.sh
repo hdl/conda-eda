08e12e7adb331eb6159b9a105856878476a507c2
Sean Cross <sean@xobs.io>
2023-02-24 11:57:56 +0800

    misc/netgen: add support for osx
    Disable X11 for Darwin builds.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- misc/netgen/build.sh
+++ misc/netgen/build.sh
@@ -7,6 +7,7 @@ set -x
 UNAME_OUT="$(uname -s)"
 case "${UNAME_OUT}" in
     Linux*)     OS=Linux;;
+    Darwin*)    OS=Mac;;
     *)          OS="${UNAME_OUT}"
                 echo "Unknown OS: ${OS}"
                 exit;;
@@ -22,12 +23,23 @@ echo "---------------------"
 
 echo CPPFLAGS="$CPPFLAGS"
 
-./configure \
-	--prefix="${PREFIX}" \
-	--with-tk="${PREFIX}" \
-	--with-tcl="${PREFIX}" \
-	--x-includes="${PREFIX}" \
-	--x-libraries="${PREFIX}"
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/autoconf/build-aux/config.* scripts
+
+if [ "$OS" = "Linux" ]; then
+	./configure \
+		--prefix="${PREFIX}" \
+		--with-tk="${PREFIX}" \
+		--with-tcl="${PREFIX}" \
+		--x-includes="${PREFIX}" \
+		--x-libraries="${PREFIX}"
+elif [ "$OS" = "Mac" ]; then
+	./configure \
+		--prefix="${PREFIX}" \
+		--with-tk="${PREFIX}" \
+		--with-tcl="${PREFIX}" \
+		--without-x
+fi
 
 echo "Configure ended with: $?"
 make V=1 -j$NUM_CORES || exit 1
